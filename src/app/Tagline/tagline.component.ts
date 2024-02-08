31c3c2d4b9589764ed7ae7472ff0b1099a1035b3
vedavathi-b26 <vedavathi.b@westernacher.com>
2024-02-08 23:24:28 +0530

    Tagline Component changes - Added

---
+++ src/app/Tagline/tagline.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-tagline',
+  templateUrl: './tagline.component.html',
+  styleUrls: ['./tagline.component.scss'],
+})
+export class TaglineComponent {
+    title: any;
+}
