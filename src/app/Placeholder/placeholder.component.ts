7134405c93c47bb9ebeb09f3fe484f7130004a53
vedavathi-b26 <vedavathi.b@westernacher.com>
2024-02-08 23:23:36 +0530

    Placeholder Component changes - Added

---
+++ src/app/Placeholder/placeholder.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-Placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.scss'],
+})
+export class PlaceholderComponent {
+  
+}
