f7dab2289727e4990f715159dad9416f566f6890
vedavathi-b26 <vedavathi.b@westernacher.com>
2024-02-08 23:22:35 +0530

    Logo Component changes - Added

---
+++ src/app/Logo/logo.component.ts
@@ -0,0 +1,14 @@
+import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss'],
+})
+export class LogoComponent implements OnInit{
+
+  ngOnInit() {
+
+  }
+}
